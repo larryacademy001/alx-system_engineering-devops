2433778a9c18d9b33551887bdedee60aa3230862
Larry Academy <larryacademy@gmail.com>
2024-04-24 07:18:25 +0100

    Little Update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,5 @@
 # This manifest creates a file at /tmp
+
 file { '/tmp/school':
     ensure  => 'file',
     owner   => 'www-data',
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# installs flask from pip3
-package { 'flask':
+#Using Puppet, install flask from pip3
+
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
