a5a84bb5023a081554fa6e1e692de7f02e34bc77
Larry Academy <larryacademy@gmail.com>
2024-05-21 05:07:58 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,11 +12,11 @@ REST_API = "https://jsonplaceholder.typicode.com"
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            user_id = int(sys.argv[1])
+            user_req = requests.get('{}/users/{}'.format(REST_API, user_id)).json()
             task_req = requests.get('{}/todos'.format(REST_API)).json()
             employee_name = user_req.get('name')
-            all_tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            all_tasks = list(filter(lambda x: x.get('userId') == user_id, task_req))
             tasks_done = list(filter(lambda x: x.get('completed'), all_tasks))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
