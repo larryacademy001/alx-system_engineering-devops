0f211ddfb2fbcb4888ecb17a5da369dd4f7ecba9
Larry Academy <larryacademy@gmail.com>
2024-05-21 05:09:07 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Get Employee Data from API
+GET employee data from API
 '''
 
 import re
@@ -12,19 +12,19 @@ REST_API = "https://jsonplaceholder.typicode.com"
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            user_id = int(sys.argv[1])
-            user_req = requests.get('{}/users/{}'.format(REST_API, user_id)).json()
+            id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
             task_req = requests.get('{}/todos'.format(REST_API)).json()
-            employee_name = user_req.get('name')
-            all_tasks = list(filter(lambda x: x.get('userId') == user_id, task_req))
-            tasks_done = list(filter(lambda x: x.get('completed'), all_tasks))
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
-                    employee_name,
-                    len(tasks_done),
-                    len(all_tasks)
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
                 )
             )
-            if len(tasks_done) > 0:
-                for task in tasks_done:
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
                     print('\t {}'.format(task.get('title')))
