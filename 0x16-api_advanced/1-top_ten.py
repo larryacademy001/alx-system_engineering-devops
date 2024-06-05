b0bdaebaa1df72c1f362bcbb5587a10d27dfadf2
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-06-05 05:54:55 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,8 +11,10 @@ def top_ten(subreddit):
     Prints the titles of the top ten hot posts for a given subreddit.
     '''
     user = {'User-Agent': 'larryacademy001'}
-    apiURL = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-        .format(subreddit), headers=user).json()
+    apiURL = requests.get(
+        'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit),
+        headers=user
+    ).json()
     try:
         for post in apiURL.get('data').get('children'):
             print(post.get('data').get('title'))
