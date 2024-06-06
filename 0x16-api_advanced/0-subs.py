e5a00043981c57378e4c9236f9b73d5385d313f3
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-06-06 07:15:31 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total subscriber count for the specified subreddit."""
-    apiURL = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
+    apiURL = "https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "larryacademy001"}
     response = requests.get(apiURL, headers=headers, allow_redirects=False)
     if response.status_code == 200:
