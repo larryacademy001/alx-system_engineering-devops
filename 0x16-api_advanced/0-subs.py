7732eb4a46f3510e1f3fe68b87ddf863a4960653
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-06-06 07:09:45 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""
-Script that queries the number of subscribers on a given
-Reddit subreddit.
-"""
-
+"""subreddit subscribers"""
 import requests
-
+import json
 
 def number_of_subscribers(subreddit):
-    """Return the total subscriber count for the specified subreddit."""
-    apiURL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "larryacademy001"}
-    response = requests.get(apiURL, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    try:
+        r = requests.get(url, allow_redirects=False)
+        if r.status_code == 200:
+            dt = r.json()
+            if 'data' in dt.keys():
+                if 'subscribers' in dt['data'].keys():
+                    return dt['data']['subscribers']
+        return 0
+    except requests.RequestException:
         return 0
