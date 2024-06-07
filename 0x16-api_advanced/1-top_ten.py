4111d58bfd65836c7148b72b48f46b3a4b988650
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-06-07 05:58:13 +0100

    UPdate

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+'''
+This module contains function top_ten.
+'''
+import requests
+from sys import argv
+
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
+    '''
+    Prints the titles of the top ten hot posts for a given subreddit.
+    '''
+    user = {'User-Agent': 'larryacademy001'}
+    apiURL = requests.get(
+        'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit),
+        headers=user
+    ).json()
+    try:
+        for post in apiURL.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+
+if __name__ == "__main__":
+    top_ten(argv[1])
